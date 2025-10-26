b79ccc61db4d190538045f5fc8a8ab7552118586
Eduardo <eduardoviniciusjorge@gmail.com>
2025-10-26 20:45:39 -0300

    1 - menssagem na tela

--- ProgComp/Primeiro.cpp
+++ ProgComp/Primeiro.cpp
@@ -1,1 +1,12 @@
-// primeiro
+// primeiro.cpp - mostra uma mensagem
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	cout << "Bem vindo ao C++";
+	cout << endl;
+	cout << "Meu primeiro programa!" << endl;
+
+	return 0;
+}                 
