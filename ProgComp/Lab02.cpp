32afd8e1d5dc8c526a98f3176eaefd66ca2c1581
Eduardo <eduardoviniciusjorge@gmail.com>
2025-10-31 17:58:06 -0300

    Q: calculo medio de cigarros fumado

--- ProgComp/Lab02.cpp
+++ ProgComp/Lab02.cpp
@@ -2,15 +2,25 @@
 #include <cstdlib>
 #include <string>
 
+
 using namespace std;
 
 int main(void)
 {
 	system("chcp 1252 > null");
 
-	int idade, dias;
-	cout << "Digite sua idade: ";
-	cin >> idade;	
-	dias = idade * 365;
-	cout << idade << " anos equivalem a " << dias << " dias.";
+	int qtdAnos, qtdCigarros;
+	double precoCarteiras, totalGasto;
+
+	cout << "A quantos anos você fuma? ";
+	cin >> qtdAnos;
+	cout << "Quantos cigarros você fuma por dia? ";
+	cin >> qtdCigarros;
+	cout << "Qual é o preço médio de uma carteira de cigarros? ";
+	cin >> precoCarteiras;
+
+	totalGasto = (qtdAnos * 365) * qtdCigarros / 20.0 * precoCarteiras;
+
+	cout << "Você gastou até agora R$" << totalGasto << " com gastos";
+
 }
