d809908e95719df2aff6fc5745fc234ed6fe2bad
Eduardo <eduardoviniciusjorge@gmail.com>
2025-11-04 02:58:55 -0300

    Q:Gerando numeros pseudoaleatorios com as funcoes rand() e srand()

--- ProgComp/Lab02.cpp
+++ ProgComp/Lab02.cpp
@@ -8,17 +8,11 @@ using namespace std;
 int main(void)
 {
 	system("chcp 1252 > null");
-	
-	int area, volume, a, b, altura, prisma;
-
-	cout << "Lado a: ";
-	cin >> a;
-	cout << "Labo b: ";
-	cin >> b;
-	area = a * b;
-	cout << "Área da base = " << area << endl;
-	cout << "Altura: ";
-	cin >> altura;
-	prisma = area * altura;
-	cout << "Volume do prisma = " << prisma << endl;
+	cout << "Gerando números psedoualeatórios:" << endl;
+	srand(1);
+	cout << rand() << endl;
+	cout << rand() << endl;
+	cout << rand() << endl;
+	cout << rand() << endl;
+	cout << rand() << endl;
 }
