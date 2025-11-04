8e414ebd8c4beba1168527a61afeca14181ee2f6
Eduardo <eduardoviniciusjorge@gmail.com>
2025-11-04 17:20:46 -0300

    Q: calculo seno de um angulo

--- ProgComp/Lab02.cpp
+++ ProgComp/Lab02.cpp
@@ -4,22 +4,15 @@
 
 using namespace std;
 
-
-
-int main(void)
+int main()
 {
-	system("chcp 1252 > null");
+    system("chcp 1252 > null");
+    double rad, angle, seno;
+    const double pi = 3.14159265358979323846;
 
-	double Qx, Qy, Px, Py;
-
-	cout << "Ponto P: " << endl;
-	cin >> Px >> Py;
-	cout << "Ponto Q: " << endl;
-	cin >> Qx >> Qy;
-	double calculo = sqrt(pow(Qx - Px, 2.0) + pow(Qy - Py, 2.0));
-	cout << endl;
-	cout << "A distância entre P e Q é: " << calculo;
+    cout << "Digite um ângulo: ";
+    cin >> angle;
+    rad = angle * (pi / 180.0);
+    seno = sin(rad);
+    cout << "Seno = " << seno << endl;  
 }
-
-
-
