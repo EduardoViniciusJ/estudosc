ac92718a019fd0fb11336e21cc45aea89ad0a3d1
Eduardo <eduardoviniciusjorge@gmail.com>
2025-11-04 16:53:20 -0300

    Q: funcoes sqrt e pow, para calcular distancias entre dois pontos

--- ProgComp/Lab02.cpp
+++ ProgComp/Lab02.cpp
@@ -1,28 +1,25 @@
 #include <iostream>
 #include <ctime>
+#include <cmath>
 
 using namespace std;
 
-void UmTres();
-void Dois();
+
 
 int main(void)
 {
 	system("chcp 1252 > null");
 
-	cout << "Começando agora" << endl;
-	UmTres();
-	cout << "Pronto!";
+	double Qx, Qy, Px, Py;
 
+	cout << "Ponto P: " << endl;
+	cin >> Px >> Py;
+	cout << "Ponto Q: " << endl;
+	cin >> Qx >> Qy;
+	double calculo = sqrt(pow(Qx - Px, 2.0) + pow(Qy - Py, 2.0));
+	cout << endl;
+	cout << "A distância entre P e Q é: " << calculo;
 }
 
-void UmTres() {
-	cout << "Um ";
-	Dois();
-	cout << "Três" << endl;;
-}
 
-void Dois() {
-	cout << "Dois ";
-}
 
