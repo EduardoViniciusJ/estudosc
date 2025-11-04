f4ee26b985fbd3a5a0efbe1e4636afbe7efd9616
Eduardo <eduardoviniciusjorge@gmail.com>
2025-11-04 18:39:47 -0300

    Q: funcoes e uso do rand()

--- ProgComp/Lab02.cpp
+++ ProgComp/Lab02.cpp
@@ -4,22 +4,44 @@
 
 using namespace std;
 
-
-int Absoluto(int value);
+int Inicializar();
+void Ligar();
+void Verificar();
+void Ativar();
 
 int main()
 {
 	system("chcp 1252 > null");
 
-	int value, absoluto;
+	int system = Inicializar();
 	
-	cout << "Digite um número inteiro: ";
-	cin >> value;
-	absoluto = Absoluto(value);
-	cout << "O valor absoluto é " << absoluto;
+	if (system > 16384) {
+		cout << endl;
+		cout << "Sistema funcionando." << endl;
+	}
+	else {
+		cout << endl;
+		cout << "Falha na inicialização." << endl;
+	}
+	
 }
 
-int Absoluto(int value) {
-	return sqrt(pow(value, 2));
+int Inicializar() {
+	cout << "Inicializando Sistema: " << endl;
+	Ligar();
+	Verificar();
+	Ativar();
+	cout << "Inicialização concluída." << endl;
+
+	return rand();
 }
 
+void Ligar() {
+	cout << " - Ligando dispositivos" << endl;
+}
+void Verificar() {
+	cout << " - Verificando integridade" << endl;
+}
+void Ativar() {
+	cout << " - Ativando Processos" << endl;
+}
