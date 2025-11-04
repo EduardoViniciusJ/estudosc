2b35da78fa4fda6964f7a7415c0e5a8dd4f8b752
Eduardo <eduardoviniciusjorge@gmail.com>
2025-11-04 18:18:35 -0300

    Q: calculo volume de um cilindro

--- ProgComp/Lab02.cpp
+++ ProgComp/Lab02.cpp
@@ -4,20 +4,31 @@
 
 using namespace std;
 
-double Media(int x, int y);
+double VolumeCilindro(double raio, double altura);
 
 int main()
 {
-    system("chcp 1252 > null");
-    int x, y;
-    cout << "Digite um valor inteiro: ";
-    cin >> x;
-    cout << "Digite o outro valor inteiro: ";
-    cin >> y;
-    cout << "A média dos números é " << Media(x, y) << endl;
+	system("chcp 1252 > null");
+	
+	double raio, altura, volume;
+	
+	cout << "Calcula o Valor de um Cilindro" << endl;
+	for (int i = 0; i < 30; i++) {
+		cout << "-";
+	}
+	cout << endl;
+	cout << "Entre com o raio da base : ";
+	cin >> raio;
+	cout << "Entre com a altura: ";
+	cin >> altura;
+	volume = VolumeCilindro(raio, altura);
+	cout << "O volume do cilindro é " << volume << endl;
+
 }
 
-double Media(int x, int y) {
-    return (x + y) / 2.0;
+double VolumeCilindro(double raio, double altura) {
+	double pi = 3.14159;
+
+	return pi * pow(raio, 2.0) * altura;
 }
 
