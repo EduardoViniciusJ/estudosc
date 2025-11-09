368829628fc3545e11b453b26995896d904974a5
Eduardo <eduardoviniciusjorge@gmail.com>
2025-11-08 23:15:56 -0300

    Q: media harmonica de dois numeros

--- ProgComp/Lab02.cpp
+++ ProgComp/Lab02.cpp
@@ -5,22 +5,25 @@
 using namespace std;
 
 
-void ConvertFahrenheit(double celsius);
+double MediaHarmonica(double x, double y);
 
 int main()
 {
 	system("chcp 1252 > null");
 
-	double celsius;
+	double x, y, media;
 
-	cout << "Digite uma temperatura em graus Celsius: ";
-	cin >> celsius;
-	ConvertFahrenheit(celsius);
+	cout << "Entre com um número: ";
+	cin >> x;
+	cout << "Entre com o outro número: ";
+	cin >> y;
+	media= MediaHarmonica(x, y);
+	cout << "A média harmônica dos números é " << media;
 }
 
-void ConvertFahrenheit(double celsius) {
+double MediaHarmonica(double x, double y) {
 
-	double calculo = 1.8 * celsius + 32.0;
-	cout << celsius << " graus Celsius equivalem a " << calculo << " graus Fahrenheit.";
+	double calculo = 2.0 * x * y / (x + y);
+	return calculo;
 }
 
