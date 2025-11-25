050fa9ff262046455c0c64d3f50b05a45e1c6cfe
Eduardo <eduardoviniciusjorge@gmail.com>
2025-11-25 14:51:47 -0300

    Q: tempo de viagem

--- ProgComp/Lab02.cpp
+++ ProgComp/Lab02.cpp
@@ -4,13 +4,28 @@
 
 using namespace std;
 
-#define ZERO 0
+#define MINUTOSPORHORA 60
 
 int main()
 {
-	int total = ZERO;
-	cout << "Inicialmente total = " << total << " (ZERO)" << endl;
-	total = 50;
-	cout << "Depois da atribuicao total = " << total << endl;
+
+	int h1, m1, h2, m2;
+	char operador;
+
+	cout << "Digite o horário de partida (HH:MM): ";
+	cin >> h1 >> operador >> m1;
+	cout << "Digite o horário de chegada (HH:MM): ";
+	cin >> h2 >> operador >> m2;
+	cout << endl;
+	int minutosPartida = h1 * MINUTOSPORHORA + m1;
+	int minutosChegada = h2 * MINUTOSPORHORA + m2;
+
+	int tempo = minutosChegada - minutosPartida;
+
+	int horas = tempo / MINUTOSPORHORA;
+	int minutos = tempo % MINUTOSPORHORA;
+
+	cout << "O tempo total de viagem foi de " << horas << " horas e " << minutos << " min." << endl;
+
 
 }
