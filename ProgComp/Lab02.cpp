859a8553b611a9656c2c988d1fc7a30fdeaeae73
Eduardo <eduardoviniciusjorge@gmail.com>
2025-11-25 12:36:53 -0300

    Q: usando o #define

--- ProgComp/Lab02.cpp
+++ ProgComp/Lab02.cpp
@@ -4,31 +4,13 @@
 
 using namespace std;
 
-double Quadrado(double valor);
-double Cubo(double valor);
-double CuboDoQuadrado(double cubo);
+#define ZERO 0
 
 int main()
 {
-	system("chcp 1252 > null");
-	double valor, quadrado;
-	cout << "Digite um valor: ";
-	cin >> valor;
-	cout << endl;
-	quadrado = Quadrado(valor);
-	cout << "Quadrado = " << quadrado << endl;
-	cout << "Cubo = " << Cubo(valor)<< endl; 
-	cout << "Cubo do Quadrado = " << CuboDoQuadrado(quadrado) << endl;
-}
+	int total = ZERO;
+	cout << "Inicialmente total = " << total << " (ZERO)" << endl;
+	total = 50;
+	cout << "Depois da atribuicao total = " << total << endl;
 
-double Quadrado(double valor) {
-	return valor * valor;
-}
-
-double Cubo(double valor) {
-	return valor * valor * valor;
-}
-
-double CuboDoQuadrado(double valor) {
-	return Cubo(valor);
 }
