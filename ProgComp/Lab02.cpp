040e769c2c110dd350d0bf3075d98abe9b15d2d1
Eduardo <eduardoviniciusjorge@gmail.com>
2025-12-20 09:38:55 -0300

    ligando, desligando e testando bits

--- ProgComp/Lab02.cpp
+++ ProgComp/Lab02.cpp
@@ -10,38 +10,48 @@ int main()
 {
 	system("chcp 1252 > null");
 
-	// Inverte os bits ou seja ele é 1 porém invertido fica 254 OPERADOR NEGATIVO ~ 
-	unsigned char estado = 1;
-	estado = ~estado;
-	cout << (int)estado << endl;
+	cout << "Ligar qual bit? ";
+
+	int bit; 
+	cin >> bit;
+	unsigned char mascara = 1 << bit;
+
+	unsigned char estado = 167;
+	estado = estado | mascara;
+	cout << int(estado) << endl;
+
+	cout << "Desligar qual bit? ";
+
+	int bit2;
+	cin >> bit2;
+	unsigned char mascara2 = ~(1 << bit2);
+
+	unsigned char estado1 = 252; 
+	estado1 = estado1 & mascara2;
+	cout << int(estado1) << endl;
+
+	cout << "Testar qual bit? ";
+	int bit3; 
+	cin >> bit3;
+	unsigned char mascara3 = 1 << bit3;
+
+	unsigned char estado3 = 240;
+	if (estado3 & mascara3) {
+		cout << "ligado " << endl;
+	}
+	else {
+		cout << "desligado " << endl;
+	}
 
 
-	// Desloca uma certa quantidade de bits para ESQUERDA, nesse caso 3 bits foram para esquerda. OPERADOR <<
-	unsigned char estado1 = 1;
-	estado1 = estado1 << 3;
-	cout << (int)estado1 << endl;
-
-	// Desloca uma certa quantidade de bits para DIREITA, nesse caso 3 bits foram para esquerda. OPERADOR >>
-	unsigned char estado2 = 8;
-	estado2 = estado2 >> 3;
-	cout << (int)estado2 << endl;
+	
 
 
 
-	// Faz um AND entre bits dos seus operandos, assim gerando um novo numero. OPERADOR &
-	unsigned char estado3 = 170;
-	estado3 = estado3 & 15;
-	cout << (int)estado3 << endl;
 
-	// Faz um OU entre bits dos seus operandos, assim gerando um novo numero. OPERADOR |
-	unsigned char estado4 = 170;
-	estado4 = estado4 | 15;
-	cout << (int)estado4 << endl;
 
-	// Faz um XOR entre bits dos seus operandos, assim gerando um novo numero. ^
-	unsigned char estado5 = 170;
-	estado5 = estado5 ^ 15;
-	cout << (int)estado5 << endl;
+
+
 
 
 
