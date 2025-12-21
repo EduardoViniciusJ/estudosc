a25c6f09f9df9faf8bb176dadbb70e49c3955eda
Eduardo <eduardoviniciusjorge@gmail.com>
2025-12-21 14:32:58 -0300

    Q: conversão detipos int para char, da tabela ASCII

--- ProgComp/Lab02.cpp
+++ ProgComp/Lab02.cpp
@@ -5,57 +5,26 @@
 
 using namespace std;
 
+char codigo(int x);
+
+
 
 int main()
 {
 	system("chcp 1252 > null");
 
-	cout << "Ligar qual bit? ";
+    cout << codigo(80);
+    cout << codigo(114);
+    cout << codigo(111);
+    cout << codigo(103);
+    cout << codigo(67);
+    cout << codigo(111);
+    cout << codigo(109);
+    cout << codigo(112);
+    cout << endl;
 
-	int bit; 
-	cin >> bit;
-	unsigned char mascara = 1 << bit;
+}
 
-	unsigned char estado = 167;
-	estado = estado | mascara;
-	cout << int(estado) << endl;
-
-	cout << "Desligar qual bit? ";
-
-	int bit2;
-	cin >> bit2;
-	unsigned char mascara2 = ~(1 << bit2);
-
-	unsigned char estado1 = 252; 
-	estado1 = estado1 & mascara2;
-	cout << int(estado1) << endl;
-
-	cout << "Testar qual bit? ";
-	int bit3; 
-	cin >> bit3;
-	unsigned char mascara3 = 1 << bit3;
-
-	unsigned char estado3 = 240;
-	if (estado3 & mascara3) {
-		cout << "ligado " << endl;
-	}
-	else {
-		cout << "desligado " << endl;
-	}
-
-
-	
-
-
-
-
-
-
-
-
-
-
-	
-
-	
+char codigo(int x) {
+	return (char)x;
 }
