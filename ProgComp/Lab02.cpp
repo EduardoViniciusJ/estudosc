227579de7524a61d737ef25d03c6b6b004c80ffe
Eduardo <eduardoviniciusjorge@gmail.com>
2025-12-25 14:33:26 -0300

    Q: exibir bits e testar bits

--- ProgComp/Lab02.cpp
+++ ProgComp/Lab02.cpp
@@ -1,26 +1,38 @@
 #include <iostream>
 using namespace std;
 
-
-int bitsBaixo(unsigned int estado);
-
-
-
+bool testarBit(unsigned char valor, int posicaoBit);
+void exibirBits(unsigned char valor);
 
 int main() {
 
-	system("chcp 1252 > null");
-						
-	int valor;
-	cout << "Digite um valor inteiro: ";
-	cin >> valor;
-	cout << "Os 16 bits mais baixos desse valor correspondem ao número " << bitsBaixo(valor) << endl;
-}		
+    system("chcp 1252 > null");
 
-int bitsBaixo(unsigned int estado) {
-	
-	return estado & 0xffff;
+    int entrada;
+    cout << "Digite um valor entre 0 e 255: ";
+    cin >> entrada;
+
+    unsigned char valor = (unsigned char)entrada;
+
+    cout << "O número " << entrada << " em binário é ";
+    exibirBits(valor);
+    cout << endl;
+
+    return 0;
 }
-				
 
+bool testarBit(unsigned char valor, int posicaoBit) {
 
+    if (posicaoBit < 0 || posicaoBit > 7) {
+        return false;
+    }
+
+    return (valor & (1 << posicaoBit)) != 0;
+}
+
+void exibirBits(unsigned char valor) {
+
+    for (int i = 7; i >= 0; i--) {
+        cout << (testarBit(valor, i) ? '1' : '0');
+    }
+}
