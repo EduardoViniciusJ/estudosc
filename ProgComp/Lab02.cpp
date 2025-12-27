be48a7a6a1208521af162dd563731cdbf4447e2a
Eduardo <eduardoviniciusjorge@gmail.com>
2025-12-27 14:47:55 -0300

    cotrole de casas decimais apos a virgula

--- ProgComp/Lab02.cpp
+++ ProgComp/Lab02.cpp
@@ -1,38 +1,18 @@
 #include <iostream>
 using namespace std;
 
-bool testarBit(unsigned char valor, int posicaoBit);
-void exibirBits(unsigned char valor);
+
 
 int main() {
 
     system("chcp 1252 > null");
 
-    int entrada;
-    cout << "Digite um valor entre 0 e 255: ";
-    cin >> entrada;
+    float f = 6.1;
 
-    unsigned char valor = (unsigned char)entrada;
+    cout << f << endl;
 
-    cout << "O número " << entrada << " em binário é ";
-    exibirBits(valor);
-    cout << endl;
+    cout << fixed;
+    cout.precision(7);
+    cout << "f = " << f << endl;
 
-    return 0;
-}
-
-bool testarBit(unsigned char valor, int posicaoBit) {
-
-    if (posicaoBit < 0 || posicaoBit > 7) {
-        return false;
-    }
-
-    return (valor & (1 << posicaoBit)) != 0;
-}
-
-void exibirBits(unsigned char valor) {
-
-    for (int i = 7; i >= 0; i--) {
-        cout << (testarBit(valor, i) ? '1' : '0');
-    }
 }
