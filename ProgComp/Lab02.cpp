695324f2ecfbdb0fea456c3d7bc3e33431779c57
Eduardo <eduardoviniciusjorge@gmail.com>
2025-12-29 22:48:43 -0300

    Q: calculos com float e double, calculo de IMC

--- ProgComp/Lab02.cpp
+++ ProgComp/Lab02.cpp
@@ -1,18 +1,35 @@
 #include <iostream>
 using namespace std;
 
+/// <summary>
+///     cout << fixed;  cout.precision(7);
+/// </summary>
+/// <returns></returns>
+
+
+float imcF(float, float);
+double imcD(double, double);
 
 
 int main() {
 
     system("chcp 1252 > null");
 
-    float f = 6.1;
+    cout << "IMC em Float: " << imcF(1.75f, 80.0f) << endl;
+    cout << "IMC em Double: " << imcD(1.75, 80.0) << endl;
 
-    cout << f << endl;
+    cout << endl;
 
-    cout << fixed;
-    cout.precision(7);
-    cout << "f = " << f << endl;
+    cout << fixed; cout.precision(10);
+    cout << "IMC em Float: " << imcF(1.75f, 80.0f) << endl;
+    cout << "IMC em Double: " << imcD(1.75, 80.0) << endl;
 
+}
+
+
+float imcF(float altura, float peso) {
+    return peso / (pow(altura, 2));
+}
+double imcD(double altura, double peso) {
+    return peso / (pow(altura, 2));
 }
