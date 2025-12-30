3fb7b939f6a7360d85037baba6b65fe22b365f26
Eduardo <eduardoviniciusjorge@gmail.com>
2025-12-29 23:06:38 -0300

    Q: notacao cientifica, decimal e padrao

--- ProgComp/Lab02.cpp
+++ ProgComp/Lab02.cpp
@@ -6,30 +6,15 @@ using namespace std;
 /// </summary>
 /// <returns></returns>
 
-
-float imcF(float, float);
-double imcD(double, double);
-
-
 int main() {
 
     system("chcp 1252 > null");
 
-    cout << "IMC em Float: " << imcF(1.75f, 80.0f) << endl;
-    cout << "IMC em Double: " << imcD(1.75, 80.0) << endl;
-
-    cout << endl;
-
-    cout << fixed; cout.precision(10);
-    cout << "IMC em Float: " << imcF(1.75f, 80.0f) << endl;
-    cout << "IMC em Double: " << imcD(1.75, 80.0) << endl;
-
+    double numero;
+    cout << "Digite um ponto flutuante: ";
+    cin >> numero;
+    cout << "Notação padrão " << numero << endl;;
+    cout << "Notação científica " << scientific << numero << endl;
+    cout << "Notação decimal " << fixed << numero << endl;
 }
 
-
-float imcF(float altura, float peso) {
-    return peso / (pow(altura, 2));
-}
-double imcD(double altura, double peso) {
-    return peso / (pow(altura, 2));
-}
